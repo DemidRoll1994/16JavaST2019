aa302c5bb4112589b5fdec282c49b7766e240b46
DemidRoll1994 <samtsov_94@mail.ru>
2019-12-07 11:46:42 +0300

    1

--- CommercialTransportConfigurator/src/main/java/by/samtsov/service/parser/Parser.java
+++ CommercialTransportConfigurator/src/main/java/by/samtsov/service/parser/Parser.java
@@ -1,7 +1,9 @@
 package by.samtsov.service.parser;
 
+import by.samtsov.bean.exceptions.IncorrectDataException;
+
 import javax.servlet.http.HttpServletRequest;
 
 public interface Parser<T> {
-    T parse(HttpServletRequest request);
+    T parse(HttpServletRequest request) throws IncorrectDataException;
 }
